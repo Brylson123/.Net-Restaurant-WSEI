6bfad2ba8dc089293c55fa68c8c506f83a83df71
Bryła <csowscy@gmail.com>
2025-01-08 17:48:24 +0100

    Change layout

--- Restaurant/Controllers/HomeController.cs
+++ Restaurant/Controllers/HomeController.cs
@@ -1,32 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Restaurant.models;
-using System.Diagnostics;
 
 namespace Restaurant.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<Uzytkownik> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<Uzytkownik> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> RedirectBasedOnRole()
         {
-            return View();
-        }
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var roles = await _userManager.GetRolesAsync(user);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+                if (roles.Contains("Admin"))
+                {
+                    return RedirectToAction("Index", "AdminPanel");
+                }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                return RedirectToAction("Create", "Zamowienia");
+            }
+
+            return RedirectToAction("Login", "Account");
         }
     }
 }
